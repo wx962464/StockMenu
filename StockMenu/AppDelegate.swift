f3e0bc16de9d47c69475dee184db3959f8ef86c4
wangxin <wangxin1420@cvte.com>
2018-10-30 22:33:38 +0800

    Initial Commit

---
+++ StockMenu/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StockMenu
+//
+//  Created by 王新 on 2018/10/30.
+//  Copyright © 2018 Aaron. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
